aca48ecbe599d8535c55a67b153d672dd1f71c0c
Kristian Sons <kristian.sons@dfki.de>
2015-10-14 23:19:03 +0200

    Use C++11 and configure compiler in travis

--- IfcPlusPlus/biicode.cmake
+++ IfcPlusPlus/biicode.cmake
@@ -4,9 +4,14 @@ ADD_BII_TARGETS()
 
 #Include biicode Boost setup
 
-set(Boost_USE_STATIC_LIBS ON)
+set(Boost_USE_STATIC_LIBS OFF)
+set(BII_BOOST_VERBOSE ON)
 
 #Use `bii_find_boost()`, our wrapper of `find_package(Boost)`:
-bii_find_boost(COMPONENTS algorithm REQUIRED)
+bii_find_boost(COMPONENTS date_time REQUIRED)
 
 target_include_directories(${BII_BLOCK_TARGET} INTERFACE ${Boost_INCLUDE_DIRS})
+
+IF(CMAKE_COMPILER_IS_GNUCXX)
+  SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+ENDIF()
